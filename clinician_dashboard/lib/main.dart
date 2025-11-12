bc825c39101edfd6fb846b944e340ba5c1df2a3f
darklorddad <danieltiong000@gmail.com>
2025-11-12 11:49:31 +0800

    fix: Update app titles to 'Florence' for consistency

--- clinician_dashboard/lib/main.dart
+++ clinician_dashboard/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BioTective Clinician Dashboard',
+      title: 'Florence Clinician Dashboard',
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
       home: const ClinicianHomeScreen(),
