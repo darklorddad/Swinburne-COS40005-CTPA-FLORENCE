5014dc6fcd47d0158ff81cb015bb4fdd2bdf0aaf
darklorddad <danieltiong000@gmail.com>
2025-11-15 02:29:39 +0800

    fix: Import missing SettingsProvider to resolve compilation error

--- florence/platform_service/lib/features/patient/dashboard/screens/dashboard_screen.dart
+++ florence/platform_service/lib/features/patient/dashboard/screens/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../../../../core/providers/settings_provider.dart';
 import '../../../../core/services/api_service.dart';
 import '../../../../core/utils/formatters.dart';
 import '../../../../core/utils/helpers.dart';
