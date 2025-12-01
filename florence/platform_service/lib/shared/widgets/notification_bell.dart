e3d52c3654633a6b1307965d58614524f12966b6
darklorddad <danieltiong000@gmail.com>
2025-12-01 13:42:21 +0800

    fix: remove invalid library statement from notification bell

--- florence/platform_service/lib/shared/widgets/notification_bell.dart
+++ florence/platform_service/lib/shared/widgets/notification_bell.dart
@@ -1,6 +1,5 @@
 /// Notification Bell Widget for FLORENCE Digital Health Platform
 /// Displays notification icon with unread count badge
-library;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
