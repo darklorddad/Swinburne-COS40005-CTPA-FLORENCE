410b7cfae7b122c2d140fe955555d6f7207e4ae3
Crazyfier <131340546+Crazyfier@users.noreply.github.com>
2025-12-03 13:59:03 +0800

    Add

--- florence/platform_service/android/settings.gradle.kts
+++ florence/platform_service/android/settings.gradle.kts
@@ -31,6 +31,8 @@ plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.firebase.appdistribution")  version "5.2.0" apply false
+
 }
 
 include(":app")
