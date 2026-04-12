45af6f8da1555301ca5debd8eea9bde1e276936b
darklorddad <danieltiong000@gmail.com>
2026-04-13 00:50:46 +0800

    refactor: remove redundant toList calls in recommendation detail screen

--- florence/platform_service/lib/features/patient/recommendations/screens/recommendation_detail_screen.dart
+++ florence/platform_service/lib/features/patient/recommendations/screens/recommendation_detail_screen.dart
@@ -359,7 +359,7 @@ class _RecommendationDetailScreenState
                   ],
                 ),
               );
-            }).toList(),
+            }),
           ],
         ),
       ),
@@ -465,7 +465,7 @@ class _RecommendationDetailScreenState
                   ],
                 ),
               );
-            }).toList(),
+            }),
           ],
         ),
       ),
